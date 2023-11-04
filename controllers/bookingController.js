0d9a5f9f61b90d0deb2fac05e4ac3917ba080455
Mohammed Adinan <adinanaries@outlook.com>
2023-11-04 11:45:23 -0400

    adding booking logs examples for data modelling

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -7,7 +7,58 @@ const getLog = (req, res, next) => {
 }
 
 const getLogs = (req, res, next) => {
-    res.status(200).send("Getting All Booking Logs of current User")
+    res.status(200).send([
+        {
+            id: "001",
+            apiProvider: "duffel",
+            bookingID: "",
+            originPayloads: [],
+            type: "flight",
+            user_id: "001",
+            airline: "American Airlines",
+            ariline_code: "",
+            trip_type: "round trip",
+            travellers: [{
+                first_name: "MOhammed",
+                last_name: "Adinan",
+                gender: "male",
+                dob: "03-23-1992",
+            }],
+            takeoff_airport: "Laguardia",
+            takeoff_airport_code: "",
+            takeoff_city: "New York",
+            destination_airport: "Kotoka",
+            destination_airport_code: "",
+            destination_city: "Accra",
+            departure_date: "03-23-2023",
+            return_date: "04-09-2023"
+        },
+        {
+            id: "001",
+            apiProvider: "duffel",
+            bookingID: "",
+            originPayloads: [],
+            type: "flight",
+            user_id: "001",
+            airline: "Virgin Airlines",
+            ariline_code: "",
+            trip_type: "round trip",
+            travellers: [{
+                first_name: "Mohammed",
+                last_name: "Adinan",
+                gender: "male",
+                dob: "03-23-1992",
+            }],
+            takeoff_airport: "JFK",
+            takeoff_airport_code: "",
+            takeoff_city: "New York",
+            destination_airport: "Charles de Gaulle Intl",
+            destination_airport_code: "",
+            destination_city: "Paris",
+            departure_date: "01-20-2023",
+            return_date: "02-11-2023"
+        }
+    ])
 }
 
 module.exports = {
