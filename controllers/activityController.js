0b17c085b040f0ba505adf87ba9e463afaed616d
Mohammed Adinan <adinanaries@outlook.com>
2023-12-09 12:46:59 -0500

    Adding Database insert function for activity logs

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user'); 
+const ActivityLog = require('../models/activityLog'); 
 const asyncHandler = require("express-async-handler");
 
 /**
@@ -9,7 +9,45 @@ const asyncHandler = require("express-async-handler");
  * @access Public
  */
 const logActivity = asyncHandler( async (req, res, next) => {
-    
+    try {
+        const { 
+            resource_id,
+            resource_type,
+            client,
+            title,
+            body,
+            type
+        } = req.body;
+
+        let activityLog = new ActivityLog({
+            resource_id: resource_id,
+            resource_type: resource_type,
+            client: client,
+            title: title,
+            body: body,
+            type: type
+        });
+        activityLog.save().then((result) => {
+            console.log(result);
+            res.status(201).send({
+                _id: result._id,
+                resource_id: result.resource_id,
+                resource_type: result.resource_type,
+                client: result.client,
+                title: result.title,
+                body: result.body,
+                type: result.type,
+            });
+        }).catch((err) => {
+            console.log(err);
+            res.status(500);
+            res.send({message: 'Activity not Logged - from server error'});
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({message: "Server Error"})
+    }
 });
 
 /**
