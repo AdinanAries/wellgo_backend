f35dac8e5088774f37753032dd0e93f3273bfed9
Mohammed Adinan <adinanaries@outlook.com>
2023-12-10 09:32:15 -0500

    logging failed bookings

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -1,5 +1,6 @@
 const ActivityLog = require('../models/activityLog');
 const ErrorLog = require('../models/errorLog');
+const FailedBookingLog = require('../models/failedBookingLog');
 
 const asyncHandler = require("express-async-handler");
 
@@ -92,7 +93,7 @@ const logError = asyncHandler( async (req, res, next) => {
         }).catch((err) => {
             console.log(err);
             res.status(500);
-            res.send({message: 'Activity not Logged - from server error'});
+            res.send({message: 'Error not Logged - from server error'});
         });
 
     } catch (err) {
@@ -109,7 +110,45 @@ const logError = asyncHandler( async (req, res, next) => {
  * @access Private
  */
 const logFailedBookings = asyncHandler( async (req, res, next) => {
-    
+    try {
+        const { 
+            resource_id,
+            resource_type,
+            client,
+            title,
+            body,
+            type
+        } = req.body;
+
+        let failedBookingLog = new FailedBookingLog({
+            resource_id: resource_id,
+            resource_type: resource_type,
+            client: client,
+            title: title,
+            body: body,
+            type: type
+        });
+        failedBookingLog.save().then((result) => {
+            console.log(result);
+            res.status(201).send({
+                _id: result._id,
+                resource_id: result.resource_id,
+                resource_type: result.resource_type,
+                client: result.client,
+                title: result.title,
+                body: result.body,
+                type: result.type,
+            });
+        }).catch((err) => {
+            console.log(err);
+            res.status(500);
+            res.send({message: 'Error not Logged - from server error'});
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({message: "Server Error"})
+    }
 });
 
 module.exports = {
