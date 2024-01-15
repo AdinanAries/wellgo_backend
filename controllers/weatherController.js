70a82e02695b56b08bdb3a47899b020e8018773d
Mohammed Adinan <adinanaries@outlook.com>
2024-01-15 05:47:04 -0500

    handling parameter errors for weather controller

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -2,6 +2,28 @@ const { get_and_preprocess_weather_data } = require("../helpers/weather");
 
 const getWeather = async (req, res, next) => {
     let params = req.params; // Should have logitudes and latitudes
+
+    // Checking if Longitude and Latitude exists
+    if(
+        !params?.longitude || !params?.latitude
+    ){
+        res.status(400).send({
+            error: true,
+            message: "Longitude and Latitude were not specified"
+        });
+        return;
+    }
+    // Checking if Dates Exist
+    if(
+        !dates?.start || !dates?.end
+    ){
+        res.status(400).send({
+            error: true,
+            message: "Start and End dates were not specified"
+        });
+        return;
+    }
+
     let dates = {
         start: params.start_date,
         end: params.end_date
