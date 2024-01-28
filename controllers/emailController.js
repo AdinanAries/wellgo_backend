fcdeaff57208cbac6d0386ee05d13893674f1598
Mohammed Adinan <adinanaries@outlook.com>
2024-01-28 16:07:17 -0500

    sent first test email

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -5,13 +5,14 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const sendEmail = (req, res, next) => {
     // Test
     const msg = {
-        to: 'recipient@example.com',
-        from: 'sender@example.com',
+        to: 'm.adinan@yahoo.com',
+        from: 'adinanaries@outlook.com',
         subject: 'Sending with SendGrid is Fun',
         text: 'and easy to do anywhere, even with Node.js',
         html: '<strong>and easy to do anywhere, even with Node.js</strong>',
     };
     sgMail.send(msg);
+    res.send("Email works!");
 }
 
 module.exports = {
