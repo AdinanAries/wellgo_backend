9533c5c7111628f57463a2f93d93a262e4abb775
Mohammed Adinan <adinanaries@outlook.com>
2024-01-29 23:02:41 -0500

    ....

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -3,16 +3,25 @@
 const stripe = require('stripe')('sk_test_51OdjZ3An0YMgH2TtcRebcqghzoyfEnf0Ezuo0HKbCvFDcSE2ECddCbGMddcCF5r5incz85NVn43mG5KkPSK9pgzh00E966NRQz');
 
 const getSecret = async (req, res, next) => {
-    let _amount = parseFloat(req?.body?.amount).toFixed(0);
-    let _currency = req?.body?.currency || 'usd';
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: _amount,
-        currency: _currency,
-        automatic_payment_methods: {
-          enabled: true,
-        },
-    });
-    res.json({client_secret: paymentIntent.client_secret});
+    try{
+        let _amount = parseFloat(req?.body?.amount).toFixed(0);
+        let _currency = req?.body?.currency || 'usd';
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: _amount,
+            currency: _currency,
+            automatic_payment_methods: {
+            enabled: true,
+            },
+        });
+        console.log(res);
+        res.json({client_secret: paymentIntent.client_secret});
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({
+            error: true,
+            message: "Server Error"
+        })
+    }
 }
 
 module.exports = {
