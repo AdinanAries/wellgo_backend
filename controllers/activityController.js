793cdbd8a7d0469d4fd265c1eb4ac9046a5b2546
Mohammed Adinan <adinanaries@outlook.com>
2024-02-01 20:26:58 -0500

    ....

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -160,8 +160,8 @@ const createBookingIntent = asyncHandler( async (req, res, next) =>{
         } = req.body;
 
         let bookingIntent = new BookingIntentLog({
-            payment_status: "",
-            booking_status: "",
+            payment_status: "initiated",
+            booking_status: "initiated",
             payment_intent: payment_intent,
             booking_order: booking_order,
         });
