ca42ebe90f158643488931c0888f5339a6fcdfc2
Mohammed Adinan <adinanaries@outlook.com>
2024-02-02 20:32:54 -0500

    adding extra validation for order flight route

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -4,6 +4,7 @@ const stripe = require('stripe')('sk_test_51OdjZ3An0YMgH2TtcRebcqghzoyfEnf0Ezuo0
 // Import application helpers
 const { return_flight_search_obj, return_duffel_order_payload  } = require("../helpers/construct_search_obj");
 const { setBookingIntentStatuses } = require("../helpers/general");
+const { markup } = require("../helpers/Prices");
 /**
  * @desc Get list of flights from data provider
  * @path POST /api/flights/
@@ -83,12 +84,12 @@ const get_offer_info = async (req, res, next) => {
  * @type controller
  */
 const create_flight_order = async (req, res, next) => {
+    let pi = req?.body?.meta?.paymentIntent;
+    let bi = req?.body?.meta?.bookingIntent;
     let flight_order;
     try{
         if(process.env.DATA_PROVIDER===constants.duffel){
             
-            let pi = req?.body?.meta?.paymentIntent;
-            let bi = req?.body?.meta?.bookingIntent;
             let payload = return_duffel_order_payload(req.body.data);
 
             // 1. Checking payment status with intent before proceeding
@@ -105,7 +106,20 @@ const create_flight_order = async (req, res, next) => {
             }
 
             // To do: Compare Intent Price Against Flight Order Price
-            // For extra layer of security
+            console.log(
+                (markup(payload?.data?.payments[0]?.amount).new_price.toFixed(0)*100),
+                paymentIntent?.amount
+            )
+            if(
+                (markup(payload?.data?.payments[0]?.amount).new_price.toFixed(0)*100)
+                !== paymentIntent?.amount
+            ){
+                res.status(500).send({message: `
+                    Server expects payment of ${(paymentIntent?.amount/100)}, and you submited
+                    ${(markup(payload?.data?.payments[0]?.amount).new_price.toFixed(0))}
+                `});
+                return;
+            }
 
             // 2. Create order from Duffel
             flight_order = await require("../flight_providers/duffel").createOrder(payload);
@@ -117,13 +131,16 @@ const create_flight_order = async (req, res, next) => {
                     // Updating booking intent statuses and booking id, and also clearing any errors
                     setBookingIntentStatuses(bi._id, "confirmed", intent?.status, flight_order?.data?.id);
                 }else {
-                    // To Do: Booking Cancellation or Payment Retry or Other action can be taken
-                    // Update Booking Intent Here - Status and OrderID, also set appropriate errors
+                    // Setting error message for Booking Intent
+                    setBookingIntentStatuses(bi._id, "failed", paymentIntent?.status, "", true, {
+                        message: "Flight Booking Failure: Not-In-Catch()"
+                    });
                 } 
             }else{
-                // If booking failed, consider cancelling the payment authorization
-                // To Do ---
-                // Also Update Booking Itent Here - Status and Error
+                // Setting error message for Booking Intent
+                setBookingIntentStatuses(bi._id, "confirmed", paymentIntent?.status, flight_order?.data?.id, true, {
+                    message: "Payment Capture Failure: Not-In-Catch()"
+                });
             }
 
             // 4. Reply to client
@@ -134,6 +151,10 @@ const create_flight_order = async (req, res, next) => {
         }
     }catch(e){
         console.log(e);
+        // Setting error message for booking intent
+        setBookingIntentStatuses(bi._id, "failed", pi?.status, "", true, {
+            message: `In Catch() Block: ${(e?.errors && e?.errors[0]?.title) || "Server Error!"}`
+        });
         res.status(500).send({message: (e?.errors && e?.errors[0]?.title) || "Server Error!"});
     }
 }
---
+++ helpers/Prices.js
@@ -0,0 +1,17 @@
+const PERCENTAGE=10;
+
+const markup = (price, percentage=PERCENTAGE) => {
+    let f_price=parseFloat(price);
+    let markup = ((percentage/100) * f_price);
+    let new_price = f_price + markup;
+    return {
+        percentage,
+        markup,
+        f_price,
+        new_price,
+    }
+}
+
+module.exports = {
+    markup
+}
