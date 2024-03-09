b6112c1cffd0d71efb00403ab3d4538b8445e2c1
Mohammed Adinan <adinanaries@outlook.com>
2024-03-09 16:43:36 -0500

    ...

--- helpers/Email.js
+++ helpers/Email.js
@@ -4,10 +4,9 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const send_email = async (payload) => {
     try{
         let sent = await sgMail.send(payload);
-        res.status(201).send(sent);
+        console.log('email sent:', sent);
     }catch(e){
         console.log(e?.response?.body?.errors);
-        res.status(500).send(e);
     }
 }
 
